0cf32a3f4375ba9aa72329cca739a195eb5b0d9f
BBK-PiJ-2015-87 <londonbim@gmail.com>
2016-02-04 19:48:16 +0000

    Print object hashcode

--- ScalaDayOne/src/RunAnimals.scala
+++ ScalaDayOne/src/RunAnimals.scala
@@ -4,7 +4,10 @@
 object RunAnimals {
   def main(args: Array[String]): Unit = {
     val tiger = new Tiger
+    val tiger2 = new Tiger
     println(tiger)
+    println(tiger2)
+    println(Integer.toHexString(tiger2.hashCode()))
     val giraffe = new Giraffe
     println(giraffe)
 
