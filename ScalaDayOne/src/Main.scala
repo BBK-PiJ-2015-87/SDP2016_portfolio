da92247624197e8208d139cbabef58f91bdf25c0
BBK-PiJ-2015-87 <londonbim@gmail.com>
2016-02-04 19:22:18 +0000

    created a method to test weather

--- ScalaDayOne/src/Main.scala
+++ ScalaDayOne/src/Main.scala
@@ -9,5 +9,23 @@ object Main {
     var text = "ABC1234"
     text = "ABC1234"
     println(text)
+    var number = 15.56
+    println(number)
+
+    def checkWeather(sky:String, tmp:Int) : Boolean ={
+      if ((sky.equals("sunny") || sky.equals("partly cloudy")) && tmp > 80) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    println(checkWeather("sunny", 82))
+    println(checkWeather("sunny", 70))
+    println(checkWeather("partly cloudy", 82))
+    println(checkWeather("partly cloudy", 70))
+    println(checkWeather("rainy", 82))
+    println(checkWeather("rainy", 72))
+
   }
 }
