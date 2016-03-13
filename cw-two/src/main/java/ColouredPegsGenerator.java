95605613709b602d1d77c9fbf9cf6cc353f8531b
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-03-13 12:34:17 +0000

    Updated coloured peg generator class

--- cw-two/src/main/java/ColouredPegsGenerator.java
+++ cw-two/src/main/java/ColouredPegsGenerator.java
@@ -1,16 +1,37 @@
+import interfaces.Colour;
+import interfaces.Generator;
+import interfaces.Peg;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Created by Workstation on 05/03/16.
+ * Created by Rustam Drake and Vladimirs Ivanovs on 05/03/16.
  */
 public class ColouredPegsGenerator implements Generator {
 
-    public List<Peg> generate(String input) {
-        return Stream.of(input.split(""))
-            .map(abbreviation -> Colour.get(abbreviation))
+    /**
+     * Generates list of colour pegs from colour list.
+     *
+     * @param colours string of abbreviations
+     * @return
+     */
+    public List<Peg> generate(List<Colour> colours) {
+        return colours.stream()
             .map(PegImpl::new)
             .collect(Collectors.toList());
+    }
+
+    /**
+     * Converts string to a list of colours.
+     *
+     * @param input string of colout abbreviations
+     * @return list of colours
+     */
+    public List<Colour> convertStringToColours(String input) {
+        return Stream.of(input.split(""))
+                .map(abbreviation -> Colour.get(abbreviation))
+                .collect(Collectors.toList());
     }
 }
--- cw-two/src/test/java/ColouredPegsGeneratorTest.java
+++ cw-two/src/test/java/ColouredPegsGeneratorTest.java
@@ -1,32 +1,30 @@
+import interfaces.Colour;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static interfaces.Colour.*;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Workstation on 05/03/16.
+ * Created by Rustam Drake and Vladimirs Ivanovs on 05/03/16.
  */
 public class ColouredPegsGeneratorTest {
     ColouredPegsGenerator generator;
-    List<Peg> expected;
+    List<Colour> colours;
 
     @Before
     public void setUp() throws Exception {
         generator = new ColouredPegsGenerator();
-        expected = new ArrayList<Peg>( );
     }
 
     @Test
     public void shouldGenerateCorrectList() throws Exception {
-        expected.add(new PegImpl(Colour.BLUE));
-        expected.add(new PegImpl(Colour.YELLOW));
-        expected.add(new PegImpl(Colour.RED));
-        expected.add(new PegImpl(Colour.PURPLE));
+        colours = Arrays.asList(BLUE, YELLOW, RED, PURPLE, GREEN, ORANGE);
+        List<Colour> result = generator.convertStringToColours("BYRPGO");
 
-        String input = "BYRP";
-        assertEquals(expected, generator.generate(input));
+        assertEquals(colours, generator.generate(result));
     }
 }
