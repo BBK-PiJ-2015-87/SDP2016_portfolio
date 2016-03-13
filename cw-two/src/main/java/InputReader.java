0377205f985a8946581a560e0ab9cfaa7812217e
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-03-13 16:39:30 +0000

    Input reader class

--- cw-two/src/main/java/InputReader.java
+++ cw-two/src/main/java/InputReader.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by Rustam Drake and Vladimirs Ivanovs on 13/03/16.
  */
 public class InputReader {
 
+    public void read(){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (input.endsWith("stop")) {
+            validate(input, 4);
+            //do stuff
+        }
+    }
 
 
     private void validate(String input, int rowNumber){
