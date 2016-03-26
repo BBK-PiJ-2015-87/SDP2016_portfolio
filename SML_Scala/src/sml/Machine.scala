cffe81bcd5feba29f7b91914576a4e512a24addd
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-03-26 14:34:03 +0000

    Machine class had program counter now to execute jump command

--- SML_Scala/src/sml/Machine.scala
+++ SML_Scala/src/sml/Machine.scala
@@ -8,17 +8,32 @@ case class Machine(labels: Labels, prog: Vector[Instruction]) {
   // The registers of the SML machine
   val regs: Registers = new Registers(NUMBEROFREGISTERS)
 
+  var PC = 0; // program counter
+
   override def toString(): String = {
     prog.foldLeft("")(_ + _)
   }
 
   // Execute the program in prog, beginning at instruction 0.
   // Precondition: the program and its labels have been store properly.
-  def execute(start: Int) =
-    start.until(prog.length).foreach(x => prog(x) execute this)
+  def execute(start: Int) ={
+    PC = start;
+    while (PC < prog.length){
+      prog(PC) execute this
+      PC += 1
+    }
+
+//      start.until(prog.length).foreach(x => prog(x) execute this)
+  }
+
 }
 
 object Machine extends App {
+  //for tests without a command line
+  override val args:Array[String] = new Array[String](3)
+  args(0) = "C:\\Users\\Workstation\\Desktop\\IdeaProjects\\Birkbeck\\SDP2016_portfolio\\SML_Scala\\src\\factorial.sml"
+
+
   if (args.length == 0) {
     println("Machine: args should be sml code file to execute")
   } else {
